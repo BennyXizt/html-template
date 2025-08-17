236b06a25bebcc7c3fc4f7ef3d7fae92638c0b6b
BennyXizt <64214352+BennyXizt@users.noreply.github.com>
2025-08-18 00:01:50 +0200

    fixed vite config

--- vite.config.ts
+++ vite.config.ts
@@ -19,7 +19,7 @@ export default defineConfig({
         '**/.DS_Store',        
         '**/*.tmp',           
         '**/.vscode/**',
-        '**/public/**'    
+        // '**/public/**'    
       ],
       usePolling: true,      
       interval: 1000          
