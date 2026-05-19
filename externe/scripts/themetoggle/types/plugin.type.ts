3a0520c170b0790f14b61fe9edeeeaf724775295
BennyXizt <64214352+BennyXizt@users.noreply.github.com>
2026-05-19 09:27:27 +0200

    [UPDATE]: Code health

--- externe/scripts/themetoggle/types/plugin.type.ts
+++ externe/scripts/themetoggle/types/plugin.type.ts
@@ -2,7 +2,6 @@ const themeArray = ['dark'] as const
 
 export type themeAttributes = typeof themeArray[number]
 
-
 export const isThemeAttribute = (value: unknown): value is themeAttributes => {
     return themeArray.includes(value as themeAttributes)
 }
