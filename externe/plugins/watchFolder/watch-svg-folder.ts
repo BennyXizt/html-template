b2197cd35d82830fca7a212612b8b175e327d302
BennyXizt <64214352+BennyXizt@users.noreply.github.com>
2026-07-17 21:59:21 +0200

    [UPDATE]: Changed fluid function

--- externe/plugins/watchFolder/watch-svg-folder.ts
+++ externe/plugins/watchFolder/watch-svg-folder.ts
@@ -70,7 +70,7 @@ function convertSVGToFile({ watchDir, filePath, nameOfTheOutputFile, translation
     
     if(isFileNotExistOrEmpty) {
         fileContent = 
-            `<svg xmlns="http://www.w3.org/2000/svg" style="display:none;">` +
+            `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" style="display:none;">` +
             `\n\t<defs>` +
             `\n\t\t<symbol id='${name}' viewBox='${viewbox}'>`
     }
