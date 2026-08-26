25f8844cacae9adf265ecbf188990137d9fbddcd
BennyXizt <64214352+BennyXizt@users.noreply.github.com>
2026-08-26 15:43:39 +0200

    [UPDATE]: Upd

--- externe/scripts/themetoggle/themetoggle.ts
+++ externe/scripts/themetoggle/themetoggle.ts
@@ -1,19 +1,10 @@
-import { type themeAttributes, isThemeAttribute } from "./types/plugin.type.js"
+import { isThemeAttribute } from "./types/plugin.type.js"
 
 export const themetogglePointerClickArray = [themetogglePointerClick, '[data-fsc-themetoggle]']
 
 
 export function themetoggleAutoload() {
-    const
-        local = localStorage.getItem('data-theme')
-    
-    if(!local) return
-
-    const 
-        root = document.querySelector('html')!
-
-    if(isThemeAttribute(local))
-        root.setAttribute('data-theme', local)
+    document.documentElement.removeAttribute('data-pre-themetoggle')
 }
 
 
